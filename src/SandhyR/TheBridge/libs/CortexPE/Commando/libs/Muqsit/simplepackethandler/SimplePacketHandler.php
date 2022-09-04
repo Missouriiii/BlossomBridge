3ae1e464ff3c64c33f09b64e86bd3b9a2d2fff5b
Tuvqlu <98550458+Tuvqlu@users.noreply.github.com>
2022-09-03 21:51:33 -0400

    Update SimplePacketHandler.php

--- src/SandhyR/TheBridge/libs/CortexPE/Commando/libs/Muqsit/simplepackethandler/SimplePacketHandler.php
+++ src/SandhyR/TheBridge/libs/CortexPE/Commando/libs/Muqsit/simplepackethandler/SimplePacketHandler.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SandhyR\TheBridge\libs\CortexPE\Commando\libs\muqsit\simplepackethandler;
+namespace SandhyR\TheBridge\libs\CortexPE\Commando\libs\muqsit\simplepackethandler\SimplePacketHandler;
 
 use InvalidArgumentException;
 use SandhyR\TheBridge\libs\CortexPE\Commando\libs\muqsit\simplepackethandler\interceptor\IPacketInterceptor;
